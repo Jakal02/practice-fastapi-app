bd690dad8ede9479a10776cf37797c9c5fbf19cf
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-20 15:07:58 -0500

    added test for simple posts index route.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,3 +5,8 @@ def test_index():
     response = client.get("/")
 
     assert {"Hello": "World"} == response.json()
+
+
+def test_posts_root():
+    response = client.get("/posts/")
+    assert {"table": "posts"} == response.json()
