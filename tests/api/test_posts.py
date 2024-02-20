f74173c548b3a62c9bd60608ee7bd0d82ed69b19
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-20 18:55:03 -0500

    CRUD tests for posts endpoints.

--- tests/api/test_posts.py
+++ tests/api/test_posts.py
@@ -1,6 +1,13 @@
+from sqlalchemy.orm import Session
+from fastapi.encoders import jsonable_encoder
 from fastapi import status
 from fastapi.testclient import TestClient
 from pydantic import PositiveInt
+from tests.conftest import _get_test_post_data, num_rows_in_tbl
+
+from app.schemas import PostRetrieve
+from app.crud.posts import posts
+from app.models import Post
 
 
 def _get_test_posts(num: PositiveInt = 1) -> dict | list[dict]:
@@ -10,28 +17,50 @@ def _get_test_posts(num: PositiveInt = 1) -> dict | list[dict]:
     return posts[0] if num == 1 else posts
 
 
-def test_create_post(client: TestClient):
-    response = client.post("/posts/", json={})
+def test_create_post(client: TestClient, db: Session):
+    post_data = _get_test_post_data()
+    response = client.post("/posts/", json=post_data.model_dump())
 
     assert response.status_code == status.HTTP_201_CREATED
+    assert num_rows_in_tbl(db, Post) == 1
 
 
-def test_delete_post(client: TestClient):
-    id = 0
+def test_delete_post(client: TestClient, db: Session):
+    post_data = _get_test_post_data()
+    made_post = posts.create(db, obj_in=post_data)
+    assert num_rows_in_tbl(db, Post) == 1
+
+    id = made_post.id
     response = client.delete(f"/posts/{id}")
 
     assert response.status_code == status.HTTP_202_ACCEPTED
+    assert num_rows_in_tbl(db, Post) == 0
+    assert response.json() == jsonable_encoder(made_post)
 
 
-def test_update_post(client: TestClient):
-    id = 0
-    response = client.put(f"/posts/{id}", json={})
+def test_update_post(client: TestClient, db: Session):
+    post_data = _get_test_post_data()
+    made_post = posts.create(db, obj_in=post_data)
+    assert num_rows_in_tbl(db, Post) == 1
+
+    id = made_post.id
+    update_data = PostRetrieve(**jsonable_encoder(made_post))
+    update_data.body = "new body."
+
+    response = client.put(f"/posts/{id}", json=jsonable_encoder(update_data))
 
     assert response.status_code == status.HTTP_200_OK
+    assert num_rows_in_tbl(db, Post) == 1
+    assert response.json() == jsonable_encoder(update_data)
 
 
-def test_read_post(client: TestClient):
-    id = 0
+def test_read_post(client: TestClient, db: Session):
+    post_data = _get_test_post_data()
+    made_post = posts.create(db, obj_in=post_data)
+    assert num_rows_in_tbl(db, Post) == 1
+
+    id = made_post.id
     response = client.get(f"/posts/{id}")
 
     assert response.status_code == status.HTTP_200_OK
+    assert response.json() == jsonable_encoder(made_post)
