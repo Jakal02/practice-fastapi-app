248bd8064ed879fa8a04ad3748d60710dd99e774
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-20 15:14:47 -0500

    reformatted tests. stubbed posts tests.

---
+++ tests/test_posts.py
@@ -0,0 +1,17 @@
+from tests.conftest import client
+
+
+def test_create_post():
+    pass
+
+
+def test_delete_post():
+    pass
+
+
+def test_update_post():
+    pass
+
+
+def test_read_post():
+    pass
