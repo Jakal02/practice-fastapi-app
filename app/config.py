4c2d6c4ac5d25ed2aa9338e4255dc6f1f5e1f0da
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-21 11:15:03 -0500

    changed config to look for postgres url.

--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class APISettings(BaseSettings):
     """Settings for this API."""
 
-    database_url: str
+    database_url: PostgresDsn
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="allow"
     )
