422d96ccfb05c6ebf06f990b84460ef6571c5da0
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-20 22:36:21 -0500

    API routes for ghost vs. real delete.

--- app/api/endpoints/posts.py
+++ app/api/endpoints/posts.py
@@ -14,7 +14,7 @@ async def create_post(db: SessionDep, data: PostCreate):
     """Create Post in database from provided data."""
     db_post = posts.create(db, obj_in=data)
 
-    return jsonable_encoder(db_post)
+    return PostRetrieve(**jsonable_encoder(db_post))
 
 
 @router.get("/{id}", response_model=PostRetrieve, status_code=status.HTTP_200_OK)
@@ -26,7 +26,7 @@ async def get_post(db: SessionDep, id: PositiveInt):
             status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found."
         )
 
-    return jsonable_encoder(existing_post)
+    return PostRetrieve(**jsonable_encoder(existing_post))
 
 
 @router.put("/{id}", response_model=PostRetrieve, status_code=status.HTTP_200_OK)
@@ -40,11 +40,29 @@ async def update_post(db: SessionDep, id: PositiveInt, update_data: PostRetrieve
 
     db_post = posts.update(db, db_obj=existing_post, obj_in=update_data)
 
-    return jsonable_encoder(db_post)
+    return PostRetrieve(**jsonable_encoder(db_post))
 
 
 @router.delete(
     "/{id}", response_model=PostRetrieve, status_code=status.HTTP_202_ACCEPTED
+)
+async def ghost_delete_post(db: SessionDep, id: PositiveInt):
+    """Ghost delete existing Post object in database with given ID."""
+    existing_post = posts.get(db, id)
+    if not existing_post:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found."
+        )
+
+    db_post = posts.update(db, db_obj=existing_post, obj_in={"is_deleted": True})
+
+    return PostRetrieve(**jsonable_encoder(db_post))
+
+
+@router.delete(
+    "/true_delete/{id}",
+    response_model=PostRetrieve,
+    status_code=status.HTTP_202_ACCEPTED,
 )
 async def delete_post(db: SessionDep, id: PositiveInt):
     """Delete existing Post object in database with given ID."""
@@ -56,4 +74,4 @@ async def delete_post(db: SessionDep, id: PositiveInt):
 
     db_post = posts.remove(db, id=id)
 
-    return jsonable_encoder(db_post)
+    return PostRetrieve(**jsonable_encoder(db_post))
