b9116c331c05dad90ceabb9a0470e17eb698b08b
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-22 15:32:58 -0500

    base crud functions made async.

--- app/crud/base.py
+++ app/crud/base.py
@@ -6,7 +6,8 @@ from typing import Any, Generic, TypeVar
 
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, PositiveInt
-from sqlalchemy.orm import Session
+from sqlalchemy import update
+from sqlalchemy.ext.asyncio import AsyncSession
 
 ModelType = TypeVar("ModelType", bound=Any)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
@@ -26,44 +27,49 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         """
         self.model = model
 
-    def get(self, db: Session, id: PositiveInt) -> ModelType | None:
+    async def get(self, db: AsyncSession, id: PositiveInt) -> ModelType | None:
         """Retrieve the object of type ModelType given it's id."""
-        return db.query(self.model).filter(self.model.id == id).first()
+        return await db.get(self.model, id)
 
-    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+    async def create(self, db: AsyncSession, *, obj_in: CreateSchemaType) -> ModelType:
         """Create object of type ModelType in database given passed in Data."""
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data)
         db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
+        await db.commit()
+        await db.refresh(db_obj)
         return db_obj
 
-    def update(
+    async def update(
         self,
-        db: Session,
+        db: AsyncSession,
         *,
-        db_obj: ModelType,
+        id: PositiveInt,
         obj_in: UpdateSchemaType | dict[str, Any],
     ) -> ModelType:
         """Update ModelType passed in with values from relevant fields found in `obj_in`."""
-        obj_data = jsonable_encoder(db_obj)
         if isinstance(obj_in, dict):
             update_data = obj_in
         else:
             update_data = obj_in.model_dump(exclude_unset=True)
         update_data.pop("id", None)
-        for field in obj_data:
-            if field in update_data:
-                setattr(db_obj, field, update_data[field])
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
 
-    def remove(self, db: Session, *, id: PositiveInt) -> ModelType:
+        update_dict = {}
+        for field in self.model.__table__.c.keys():
+            if field in update_data:
+                update_dict[field] = update_data.get(field)
+
+        update_stmt = (
+            update(self.model).where(self.model.id == id).values(**update_dict)
+        )
+
+        await db.execute(update_stmt)
+        await db.commit()
+        return await db.get(self.model, id)
+
+    async def remove(self, db: AsyncSession, *, id: PositiveInt) -> ModelType:
         """Remove object from ModelType with id == `id`."""
-        obj = db.query(self.model).filter(self.model.id == id).first()
-        db.delete(obj)
-        db.commit()
-        return obj
+        result_obj = await db.get(self.model, id)
+        await db.delete(result_obj)
+        await db.commit()
+        return result_obj
