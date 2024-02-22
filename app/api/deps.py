3827348480640be67baa3745be9727769216d5c0
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-22 14:58:18 -0500

    async db dependency and updated calls to CRUD methods.

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,18 +1,8 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database import SessionLocal
+from app.database import get_db_session
 
-
-def get_db():
-    """Return Generator for database connections."""
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-SessionDep = Annotated[Session, Depends(get_db)]
+DBSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
--- app/api/endpoints/posts.py
+++ app/api/endpoints/posts.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from pydantic import PositiveInt
 
-from app.api.deps import SessionDep
+from app.api.deps import DBSessionDep
 from app.crud.posts import posts
 from app.schemas import PostCreate, PostRetrieve, PostUpdate
 
@@ -10,17 +10,17 @@ router = APIRouter()
 
 
 @router.post("/", response_model=PostRetrieve, status_code=status.HTTP_201_CREATED)
-async def create_post(db: SessionDep, data: PostCreate):
+async def create_post(db: DBSessionDep, data: PostCreate):
     """Create Post in database from provided data."""
-    db_post = posts.create(db, obj_in=data)
+    db_post = await posts.create(db, obj_in=data)
 
     return PostRetrieve(**jsonable_encoder(db_post))
 
 
 @router.get("/{id}", response_model=PostRetrieve, status_code=status.HTTP_200_OK)
-async def get_post(db: SessionDep, id: PositiveInt):
+async def get_post(db: DBSessionDep, id: PositiveInt):
     """Retrieve existing Post object in database with given ID."""
-    existing_post = posts.get(db, id)
+    existing_post = await posts.get(db, id)
     if not existing_post or existing_post.is_deleted:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found."
@@ -30,15 +30,15 @@ async def get_post(db: SessionDep, id: PositiveInt):
 
 
 @router.put("/{id}", response_model=PostRetrieve, status_code=status.HTTP_200_OK)
-async def update_post(db: SessionDep, id: PositiveInt, update_data: PostUpdate):
+async def update_post(db: DBSessionDep, id: PositiveInt, update_data: PostUpdate):
     """Update existing Post object in database with provided data."""
-    existing_post = posts.get(db, id)
+    existing_post = await posts.get(db, id)
     if not existing_post or existing_post.is_deleted:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found."
         )
 
-    db_post = posts.update(db, db_obj=existing_post, obj_in=update_data)
+    db_post = await posts.update(db, id=id, obj_in=update_data)
 
     return PostRetrieve(**jsonable_encoder(db_post))
 
@@ -46,15 +46,15 @@ async def update_post(db: SessionDep, id: PositiveInt, update_data: PostUpdate):
 @router.delete(
     "/{id}", response_model=PostRetrieve, status_code=status.HTTP_202_ACCEPTED
 )
-async def ghost_delete_post(db: SessionDep, id: PositiveInt):
+async def ghost_delete_post(db: DBSessionDep, id: PositiveInt):
     """Ghost delete existing Post object in database with given ID."""
-    existing_post = posts.get(db, id)
+    existing_post = await posts.get(db, id)
     if not existing_post or existing_post.is_deleted:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found."
         )
 
-    db_post = posts.update(db, db_obj=existing_post, obj_in={"is_deleted": True})
+    db_post = await posts.update(db, id=id, obj_in={"is_deleted": True})
 
     return PostRetrieve(**jsonable_encoder(db_post))
 
@@ -64,14 +64,13 @@ async def ghost_delete_post(db: SessionDep, id: PositiveInt):
     response_model=PostRetrieve,
     status_code=status.HTTP_202_ACCEPTED,
 )
-async def delete_post(db: SessionDep, id: PositiveInt):
+async def delete_post(db: DBSessionDep, id: PositiveInt):
     """Delete existing Post object in database with given ID."""
-    existing_post = posts.get(db, id)
+    existing_post = await posts.get(db, id)
     if not existing_post:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found."
         )
 
-    db_post = posts.remove(db, id=id)
-
+    db_post = await posts.remove(db, id=id)
     return PostRetrieve(**jsonable_encoder(db_post))
