bf8a06c12b5d39d26ca22dade17ec1ad73fbb3de
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-23 12:48:01 -0500

    prevent potential update injection via pure dict update.

--- app/crud/posts.py
+++ app/crud/posts.py
@@ -6,5 +6,11 @@ from app.schemas import PostCreate, PostUpdate
 class CRUDPost(CRUDBase[Post, PostCreate, PostUpdate]):
     """Subclass of CRUDBase for Posts model."""
 
+    def __init__(self, *args, **kwargs):
+        """Initialize the class."""
+        super().__init__(*args, **kwargs)
+        off_limits_fields = []
+        self.update_excluded_fields.update(off_limits_fields)
+
 
 posts = CRUDPost(Post)
