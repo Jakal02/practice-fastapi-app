b71b7f1aa5feb3fadcd41e2954f3fc8610a58e1e
Jakal02 <gitcontribs@eligrubbs.com>
2024-03-04 17:14:44 -0500

    crud functions to select many posts on time and ghost deleted.

--- app/crud/posts.py
+++ app/crud/posts.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from sqlalchemy import ColumnOperators as co
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -17,11 +18,30 @@ class CRUDPost(CRUDBase[Post, PostCreate, PostUpdate]):
         off_limits_fields = []
         self.update_excluded_fields.update(off_limits_fields)
 
-    async def all_posts_modified_since(self, db: AsyncSession, time: datetime):
+    async def all_posts_modified_since(
+        self, db: AsyncSession, time: datetime
+    ) -> list[Post]:
         """Return all posts not ghost deleted modified after `time`."""
-        stmt = select(Post).where(Post.is_deleted is False, Post.date_modified >= time)
+        stmt = (
+            select(Post)
+            .where(co.__eq__(Post.is_deleted, False))
+            .where(co.__gt__(Post.date_modified, time))
+        )
         results = await db.execute(stmt)
-        return results.all()
+        return [item.tuple()[0] for item in results.all()]
+
+    async def all_posts(
+        self, db: AsyncSession, exclude_ghost: bool = True
+    ) -> list[Post]:
+        """Return all posts.
+
+        By defualt, exclude ghost deleted posts.
+        """
+        stmt = select(Post)
+        if not exclude_ghost:
+            stmt = stmt.where(co.__eq__(Post.is_deleted, False))
+        results = await db.execute(stmt)
+        return [item.tuple()[0] for item in results.all()]
 
 
 posts = CRUDPost(Post)
