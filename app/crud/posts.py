f95b47b83002c9c67ed39222d91fdea2b6691ab7
Jakal02 <gitcontribs@eligrubbs.com>
2024-03-04 16:00:50 -0500

    added function to get all posts not ghost deleted and modified past a certain time.

--- app/crud/posts.py
+++ app/crud/posts.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
 from app.models import Post
 from app.schemas import PostCreate, PostUpdate
@@ -11,6 +16,12 @@ class CRUDPost(CRUDBase[Post, PostCreate, PostUpdate]):
         super().__init__(*args, **kwargs)
         off_limits_fields = []
         self.update_excluded_fields.update(off_limits_fields)
+
+    async def all_posts_modified_since(self, db: AsyncSession, time: datetime):
+        """Return all posts not ghost deleted modified after `time`."""
+        stmt = select(Post).where(Post.is_deleted is False, Post.date_modified >= time)
+        results = await db.execute(stmt)
+        return results.all()
 
 
 posts = CRUDPost(Post)
