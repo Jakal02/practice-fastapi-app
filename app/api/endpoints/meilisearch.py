c91c6db2900fd98315e866ab3a83df6958748f19
Jakal02 <gitcontribs@eligrubbs.com>
2024-03-05 12:34:35 -0500

    add route to search posts index.

--- app/api/endpoints/meilisearch.py
+++ app/api/endpoints/meilisearch.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from meilisearch_python_sdk.models.health import Health
 
 from app.api.deps import MeiliSessionDep
+from app.database import INDEX_NAME
 
 router = APIRouter()
 
@@ -10,3 +11,9 @@ router = APIRouter()
 async def get_meili_health(client: MeiliSessionDep):
     """Display health information of meilisearch."""
     return await client.health()
+
+
+@router.get("/search")
+async def search_for_posts(client: MeiliSessionDep):
+    """Search for posts that have been made."""
+    return await client.index(uid=INDEX_NAME).search()
